95be560b271e6aa2ee34d5a0d098741ec73e7a1f
NguyenHuyenTrang070899 <37436902+NguyenHuyenTrang070899@users.noreply.github.com>
2020-12-08 10:39:25 +0700

    Update Bridge.java

--- DesignPattern/src/Bridge.java
+++ DesignPattern/src/Bridge.java
@@ -2,8 +2,6 @@ public class Bridge {
     public static void main(String[] args) {
         Abstract a1 = new Abstract1(new Implement1());
         a1.A();
-        Abstract a2 = new Abstract2(new Implement2());
-        a2.A();
     }
 }
 interface Implement {
@@ -14,11 +12,7 @@ class Implement1 implements Implement {
     public void A() {
     }
 }
-class Implement2 implements Implement {
-    @Override
-    public void A() {
-    }
-}
+
 abstract class Abstract {
     protected Implement implement;
     public Abstract(Implement implement) {
@@ -29,15 +23,6 @@ abstract class Abstract {
 class Abstract1 extends Abstract {
 
     public Abstract1(Implement implement) {
-        super(implement);
-    }
-    @Override
-    public void A() {
-        implement.A();
-    }
-}
-class Abstract2 extends Abstract {
-    public Abstract2(Implement implement) {
         super(implement);
     }
     @Override
