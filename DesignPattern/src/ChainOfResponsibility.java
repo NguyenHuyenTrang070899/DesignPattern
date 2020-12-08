747de8929ab1882c121b906802ab3aed61623144
NguyenHuyenTrang070899 <37436902+NguyenHuyenTrang070899@users.noreply.github.com>
2020-12-08 11:22:11 +0700

    Update ChainOfResponsibility.java

--- DesignPattern/src/ChainOfResponsibility.java
+++ DesignPattern/src/ChainOfResponsibility.java
@@ -1,59 +1,30 @@
 public class ChainOfResponsibility {
     public static void main(String[] args) {
-        LeaveRequestWorkFlow.getApprover().approveLeave(new LeaveRequest(3));
-        System.out.println("---");
-        LeaveRequestWorkFlow.getApprover().approveLeave(new LeaveRequest(5));
+        WorkFlow.getB().method(new A("TT"));
     }
 }
-class LeaveRequest {
-    private int days;
-    public LeaveRequest(int days) {
-        this.days = days;
-    }
-    public int getDays() {
-        return days;
+class A {
+    private String name;
+    public A(String name) {
+        this.name = name;
     }
 }
-abstract class Approver {
-    protected Approver nextApprover;
-    public void approveLeave(LeaveRequest request) {
-        if (this.canApprove(request.getDays())) {
-            this.doApproving(request);
-        } else if (nextApprover != null) {
-            nextApprover.approveLeave(request);
-        }
+abstract class B {
+    protected B nextB;
+    public void setNext(B b) {
+        this.nextB = b;
     }
-    public void setNext(Approver approver) {
-        this.nextApprover = approver;
-    }
-    protected abstract boolean canApprove(int numberOfDays);
-    protected abstract void doApproving(LeaveRequest request);
-}
-class Supervisor extends Approver {
-    @Override
-    protected boolean canApprove(int numberOfDays) {
-        return numberOfDays <= 3;
-    }
-    @Override
-    protected void doApproving(LeaveRequest request) {
-        System.out.println("Leave request approved for " + request.getDays() + " days by Supervisor");
+    public void method(A a) {
     }
 }
-class DeliveryManager extends Approver {
-    @Override
-    protected boolean canApprove(int numberOfDays) {
-        return numberOfDays > 3;
-    }
-    @Override
-    protected void doApproving(LeaveRequest request) {
-        System.out.println("Leave request approved for " + request.getDays() + " days by Delivery Manager");
-    }
-}
-class LeaveRequestWorkFlow {
-    public static Approver getApprover() {
-        Approver supervisor = new Supervisor();
-        Approver manager = new DeliveryManager();
-        supervisor.setNext(manager);
-        return supervisor;
+class B1 extends B { }
+class B2 extends B { }
+class WorkFlow {
+    public static B getB() {
+        B b1 = new B1();
+        B b2 = new B2();
+        b1.setNext(b2);
+        return b1;
     }
 }
+    
